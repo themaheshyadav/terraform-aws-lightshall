731135cbe29864c5a1970a82f281089343495963
mahesh yadav <devopsmahesh.it@gmail.com>
2021-11-23 06:47:53 +0530

    aws-lightsail_instance-with-nginx

--- _example/example.tf
+++ _example/example.tf
@@ -1,20 +1,18 @@
-# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
-
-module "aws_lightsail_instance" {
-  source            = "../"
-  enabled           = true
-  keypair_enabled   = true
-  key_name          = "prod"
-  lightsail_enabled = true
-  light_name        = "dev-light"
+module "lightsail_instance" {
+  source = "../"
   availability_zone = "us-east-1a"
-  blueprint_id      = "ubuntu_20_04"
-  bundle_id         = "nano_2_0"
-   user_data = file("${path.module}/pritunl.sh")
-static_ip_name    = "dev-ipd"
-  static_ip_enabled = true
-public_key = "xxxxxxxxxx"
+  blueprint_id = "ubuntu_20_04"
+  bundle_id = "nano_2_0"
+  name = "dev"
+  key_name = "main_key"
+  public_key ="xyz"
+  static_ip_name = "staticip"
+  domain_name = "mydomain.com"
+  user_data = file("${path.module}./user_data")
+  tags       = {
+    created_by = "Mahesh"
+  }
 }
--- output.tf
+++ output.tf
@@ -1,4 +1,25 @@
-output "publc" {
-  value = aws_lightsail_key_pair.main.*.
-  description = "The ARN of the Lightsail instance."
+output "availability_zone" {
+  value = aws_lightsail_instance.main.availability_zone
 }
+
+
+output "tags" {
+  value = aws_lightsail_instance.main.tags
+}
+
+output "public_key" {
+  value = aws_key_pair.key.public_key
+}
+output "public_id" {
+  value = aws_key_pair.key.id
+}
+output "static_ip" {
+  value = aws_lightsail_static_ip.static.ip_address
+}
+output "domain_name" {
+  value = aws_lightsail_domain.domain_test.id
+}
+output "public_ports" {
+  value = aws_lightsail_instance_public_ports.public_ports.id
+}
+
